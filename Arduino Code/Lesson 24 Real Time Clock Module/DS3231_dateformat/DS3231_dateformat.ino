3e938ec69ab1720603d634ce2ef65d9b79db3321
Lonten Technology <142191915+LontenTechnology@users.noreply.github.com>
2023-08-23 16:12:24 +0800

    Add files via upload

---
+++ Arduino Code/Lesson 24 Real Time Clock Module/DS3231_dateformat/DS3231_dateformat.ino
@@ -0,0 +1,40 @@
+
+#include <Wire.h>
+#include <DS3231.h>
+
+DS3231 clock;
+RTCDateTime dt;
+
+void setup()
+{
+  Serial.begin(9600);
+
+  // Initialize DS3231
+  Serial.println("Initialize DS3231");
+  clock.begin();
+
+  // Set sketch compiling time
+  clock.setDateTime(__DATE__, __TIME__);
+
+  // Set from UNIX timestamp
+  // clock.setDateTime(1397408400);
+
+  // Manual (YYYY, MM, DD, HH, II, SS
+  //clock.setDateTime(2016, 12, 9, 11, 46, 00);
+}
+
+void loop()
+{
+  dt = clock.getDateTime();
+  Serial.print("Today is:                    ");
+  Serial.print(dt.day);   Serial.print("-");
+  Serial.print(dt.month);  Serial.print("-");
+  Serial.print(dt.year);    Serial.print(" ");
+  Serial.print(dt.hour);   Serial.print(":");
+  Serial.print(dt.minute); Serial.print(":");
+  Serial.print(dt.second); Serial.println("");
+  Serial.print("Unixtime:                    ");
+  Serial.println(dt.unixtime);
+  delay(1000);
+}
+
